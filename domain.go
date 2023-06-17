5ccb95aa57f9b08938c4d3469795fc6f772778bc
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-06-17 14:58:33 +0330

    events invloved in aggregate

--- domain.go
+++ domain.go
@@ -1,7 +1,18 @@
 package hexago
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type AggRoot interface {
 	Id() uuid.UUID
+	Pull() []Event
+}
+
+type Event struct {
+	Identifier uuid.UUID
+	Name       string
+	At         time.Time
+	Payload    interface{}
 }
