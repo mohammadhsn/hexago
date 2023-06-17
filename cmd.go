63cf86e0b9f72665f9877adaaea70e6b1cd48511
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-06-17 16:36:33 +0330

    add cmd bag to facilitate bus implementations

--- cmd.go
+++ cmd.go
@@ -1,5 +1,7 @@
 package hexago
 
+import "fmt"
+
 // Cmd is just Command's abbreviation, in the clean architecture there's a layer
 // which is responsible for handling system use cases. It is common to call this
 // layer `application` layer. It acts like an orchestrator which connects
@@ -16,4 +18,30 @@ type Cmd struct {
 // aggregates, and using the domain functionalities to handling use cases.
 type CmdHandler interface {
 	Handle(Cmd) (string, error)
+}
+
+// CmdHandlerFactory is a function that creates CmdHandler.
+type CmdHandlerFactory func() CmdHandler
+
+// CmdBag just holds a mapping between Cmd and CmdHandlerFactory
+type CmdBag struct {
+	conf map[string]CmdHandlerFactory
+}
+
+// NewCmdBag creates an empty CmdBag.
+func NewCmdBag() *CmdBag {
+	return &CmdBag{make(map[string]CmdHandlerFactory)}
+}
+
+func (cb *CmdBag) Add(cmd string, f CmdHandlerFactory) {
+	cb.conf[cmd] = f
+}
+
+func (cb *CmdBag) FactoryFor(cmd string) (CmdHandlerFactory, error) {
+	f, prs := cb.conf[cmd]
+	if !prs {
+		return nil, fmt.Errorf("command %s is not added yet", cmd)
+	}
+
+	return f, nil
 }
