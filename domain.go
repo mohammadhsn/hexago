ce2cf406a1beb3a767df5fc58d121f3b40c89f68
Mohammad Hosseini <hosseini.mohamad94@gmail.com>
2023-06-17 15:23:29 +0330

    More descriptions

--- domain.go
+++ domain.go
@@ -5,11 +5,16 @@ import (
 	"time"
 )
 
+// AggRoot is just an abbreviation for AggregateRoot. In DDD terms it clusters domain
+// entities so that they act like one single unit. It has ability to capturing events.
+// The brain of the business and handling core considerations goes here in AggRoot.
 type AggRoot interface {
 	Identifiable
 	Pull() []Event
 }
 
+// Event is like an important happening in the domain. It can be implied a change state,
+// or something like that.
 type Event struct {
 	Identifier uuid.UUID
 	Name       string
@@ -17,6 +22,7 @@ type Event struct {
 	Payload    interface{}
 }
 
+// Repo is responsible take and bring the domain objects and AggRoot's.
 type Repo interface {
 	Seen() []AggRoot
 }
